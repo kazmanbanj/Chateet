85129dc3dc245a8560c08e9af19f714e138485f4
kazmanbanj <58645797+kazmanbanj@users.noreply.github.com>
2020-09-27 04:36:18 -0800

    done

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', ''),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'chateet'),
             'username' => env('DB_USERNAME', 'chateet'),
