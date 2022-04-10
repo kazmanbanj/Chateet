d2fe4968bef59485ff65ac0aac8950568581135d
Kazeem Banjoko <banjoko10@gmail.com>
2022-04-10 22:01:51 +0100

    fixed image upload issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,19 +13,19 @@ class User extends Authenticatable
     use Notifiable, Followable;
 
     protected $guarded = [];
-    
+
     protected $hidden = [
         'password', 'remember_token',
     ];
-    
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    public function getAvatarAttribute($value)
-    {
-        return asset($value ?: '/images/test1.jpg');
-    }
+    // public function getAvatarAttribute($value)
+    // {
+    //     return asset($value ?: '/images/test1.jpg');
+    // }
 
     public function timeline()
     {
@@ -46,7 +46,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class);
     }
-    
+
     public function path($append = '')
     {
         $path = route('profile', $this->username);
