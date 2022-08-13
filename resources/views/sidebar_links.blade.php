c7ddcaec664ee1165c3130d0ee685ce9a89fa007
Kazeem Banjoko <banjoko10@gmail.com>
2022-08-13 21:17:26 +0100

    update

--- resources/views/sidebar_links.blade.php
+++ resources/views/sidebar_links.blade.php
@@ -1,4 +1,3 @@
-<!-- <h2>{{date('Y-m-d H:i:s')}}</h2> -->
 <ul>
     <li><a href="{{ route('home') }}" class="font-bold text-lg mb-4 block">Home</a></li>
     <li><a href="/explore" class="font-bold text-lg mb-4 block">Explore</a></li>
@@ -10,9 +9,6 @@
 
     <li><a href="{{ route('profile', auth()->user()) }}" class="font-bold text-lg mb-4 block">Profile</a></li>
     <li><a href='/notifications' class="font-bold text-lg mb-4 block">Notifications</a></li>
-
-    <!-- <li><a href="#" class="font-bold text-lg mb-4 block">Settings</a></li> -->
-    
     <li>
         <form action="/logout" method="post">
         @csrf
